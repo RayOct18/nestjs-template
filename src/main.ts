0e9483317191a851200af0462f8aeb00b816b761
rayxie <birken1018@gmail.com>
2025-01-24 10:33:02 +0800

    feat: add Swagger documentation and environment-based configuration

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { INestApplication } from '@nestjs/common';
+import { SwaggerModule } from '@nestjs/swagger';
+import { DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+
+function setupSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('NestJS Template')
+    .setDescription('The NestJS Template API description')
+    .setVersion('1.0.0')
+    .addTag('NestJS Template')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
-  app.use(helmet());
-  app.enableCors();
+  if (configService.get('NODE_ENV') === 'production') {
+    app.use(helmet());
+    app.enableCors();
+  } else {
+    setupSwagger(app);
+  }
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(configService.get('PORT') ?? 3000);
 }
 bootstrap();
